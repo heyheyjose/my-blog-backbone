e5f2af3f62db56835b669abfa5e5ac098234609f
heyheyjose <heyheyjose@users.noreply.github.com>
2014-11-01 13:07:39 -0400

    added post model

---
+++ app/scripts/models/post.js
@@ -0,0 +1,26 @@
+/*global MyBlogBackbone, Backbone*/
+
+MyBlogBackbone.Models = MyBlogBackbone.Models || {};
+
+(function () {
+    'use strict';
+
+    MyBlogBackbone.Models.Post = Backbone.Model.extend({
+
+        url: 'https://tiny-pizza-server.herokuapp.com/collections/jv-finally-damn',
+
+        initialize: function() {
+        },
+
+        defaults: {
+        },
+
+        validate: function(attrs, options) {
+        },
+
+        parse: function(response, options)  {
+            return response;
+        }
+    });
+
+})();
